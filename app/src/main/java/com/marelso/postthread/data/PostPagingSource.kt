dd1cc20ed4ada97f4fcdbc7f85a6a7452699a92c
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-26 19:00:49 -0300

    Updating paging source

--- app/src/main/java/com/marelso/postthread/data/PostPagingSource.kt
+++ app/src/main/java/com/marelso/postthread/data/PostPagingSource.kt
@@ -7,15 +7,15 @@ import com.marelso.postthread.data.Constants.PAGE_SIZE
 class PostPagingSource(private val service: PostService): PagingSource<Int, Post>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Post> {
         return try {
-            val nextPage = params.key ?: 0
-            val response = service.get(page = nextPage, size = PAGE_SIZE)
+            val currentPage = params.key ?: 0
+            val response = service.get(page = currentPage, size = PAGE_SIZE)
 
             val items: List<Post> = response.body()?.content ?: listOf()
 
             LoadResult.Page(
                 data = items,
-                prevKey = if (nextPage == 1) null else nextPage - 1,
-                nextKey = if (items.isEmpty()) null else nextPage + 1
+                prevKey = if (currentPage <= 1) null else currentPage - 1,
+                nextKey = if (items.isEmpty()) null else currentPage + 1
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
