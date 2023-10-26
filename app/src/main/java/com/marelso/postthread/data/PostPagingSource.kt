870b130ff67b76bdc0095825106b4a7d9bab6add
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-26 15:15:53 -0300

    Setting up paging 3

--- app/src/main/java/com/marelso/postthread/data/PostPagingSource.kt
+++ app/src/main/java/com/marelso/postthread/data/PostPagingSource.kt
@@ -1,4 +1,28 @@
 package com.marelso.postthread.data
 
-class PostPagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.marelso.postthread.data.Constants.PAGE_SIZE
+
+class PostPagingSource(private val service: PostService): PagingSource<Int, Post>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Post> {
+        return try {
+            val nextPage = params.key ?: 1
+            val response = service.get(page = nextPage, size = PAGE_SIZE)
+
+            val items: List<Post> = response.body()?.content ?: listOf()
+
+            LoadResult.Page(
+                data = items,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (items.isEmpty()) null else nextPage + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Post>): Int? {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/marelso/postthread/ui/ListViewModel.kt
+++ app/src/main/java/com/marelso/postthread/ui/ListViewModel.kt
@@ -1,8 +1,20 @@
 package com.marelso.postthread.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.marelso.postthread.data.Post
 import com.marelso.postthread.data.PostApi
+import com.marelso.postthread.data.PostPagingSource
+import com.marelso.postthread.data.PostService
+import kotlinx.coroutines.flow.Flow
 
-class ListViewModel(): ViewModel() {
+class ListViewModel(private val service: PostService): ViewModel() {
     private val postApiService = PostApi.instance
+    val pagingData = Pager(PagingConfig(pageSize = 20)) {
+        PostPagingSource(service)
+    }.flow.cachedIn(viewModelScope)
 }
