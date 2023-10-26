f8987ad718047377eb11b983d3b9ff7aa310abe9
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-26 15:14:36 -0300

    Setting up Koin

--- app/src/main/java/com/marelso/postthread/MainActivity.kt
+++ app/src/main/java/com/marelso/postthread/MainActivity.kt
@@ -6,38 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.marelso.postthread.ui.ListViewModel
+import com.marelso.postthread.ui.PostList
 import com.marelso.postthread.ui.theme.PostThreadTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModel<ListViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PostThreadTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    PostList(viewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PostThreadTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/marelso/postthread/di/module.kt
+++ app/src/main/java/com/marelso/postthread/di/module.kt
@@ -1,9 +1,27 @@
 package com.marelso.postthread.di
 
+import com.marelso.postthread.data.Constants.PostAPI
+import com.marelso.postthread.data.PostService
 import com.marelso.postthread.ui.ListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private fun provideRetrofitInstance(
+    url: String
+): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(url)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
 
 val appModule = module {
-    viewModel { ListViewModel() }
+    factory(named(PostAPI.NAME)) {
+        provideRetrofitInstance(PostAPI.URL)
+    }
+    factory { get<Retrofit>(named(PostAPI.NAME)).create(PostService::class.java) }
+    viewModel { ListViewModel(get()) }
 }
