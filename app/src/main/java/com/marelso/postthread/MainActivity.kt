7093e2e74caaad48e9773dc819a059d07213732c
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-28 09:35:13 -0300

    Including app bar with back navigation and post status change

--- app/src/main/java/com/marelso/postthread/MainActivity.kt
+++ app/src/main/java/com/marelso/postthread/MainActivity.kt
@@ -1,8 +1,11 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.marelso.postthread
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,16 +14,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,6 +43,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.marelso.postthread.data.Post
 import com.marelso.postthread.ui.PostDetailViewModel
 import com.marelso.postthread.ui.PostListViewModel
 import com.marelso.postthread.ui.PostList
@@ -95,7 +107,10 @@ fun goToDetail(reference: Int, navHostController: NavHostController) {
 }
 
 @Composable
-fun PostDetailScreen(viewModel: PostDetailViewModel) {
+fun PostDetailScreen(
+    viewModel: PostDetailViewModel,
+    navHostController: NavHostController
+) {
     viewModel.fetchContent()
     val postUiState = viewModel.postUiState.collectAsState()
 
@@ -114,7 +129,64 @@ fun PostDetailScreen(viewModel: PostDetailViewModel) {
 
         is PostUiState.Success -> {
             val post = (postUiState.value as PostUiState.Success).post
+
+            OnSuccess(
+                post = post,
+                goBack = { goBack(navHostController, Screen.Home) },
+                updateStatus = {
+                    changePostStatus(it, viewModel)
+                }
+            )
         }
+    }
+}
+
+fun goBack(navHostController: NavHostController, screen: Screen) {
+    navHostController.navigate(screen.route) {
+        popUpTo(screen.route) {
+            inclusive = true
+        }
+    }
+}
+
+fun changePostStatus(status: Boolean, viewModel: PostDetailViewModel) {
+    viewModel.changePostStatus(status)
+}
+
+@Composable
+fun OnSuccess(
+    post: Post,
+    updateStatus: (Boolean) -> Unit,
+    goBack: (Unit) -> Unit
+) {
+    var status by remember { mutableStateOf(post.status) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp)
+    ) {
+        TopAppBar(
+            modifier = Modifier.fillMaxWidth(),
+            title = { Text(text = post.headline) },
+            navigationIcon = {
+                Icon(
+                    modifier = Modifier.clickable { goBack.invoke(Unit) },
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(id = R.string.warning),
+                )
+            },
+            actions = {
+                Switch(
+                    checked = status,
+                    onCheckedChange = {
+                        status = it
+                        updateStatus.invoke(status)
+                    },
+                    modifier = Modifier.padding(end = 16.dp)
+                )
+            }
+        )
     }
 }
 
