310c1fb3f63e92d932a471f66dd290aa98c73f9f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-10-28 15:48:20 -0300

    Updating nav graph to handle creation screen

--- app/src/main/java/com/marelso/postthread/ui/NavGraph.kt
+++ app/src/main/java/com/marelso/postthread/ui/NavGraph.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.marelso.postthread.CreatePostScreen
 import com.marelso.postthread.HomeScreen
 import com.marelso.postthread.PostDetailScreen
 import org.koin.androidx.compose.getViewModel
@@ -19,6 +20,7 @@ const val KEY_DETAIL_SCREEN = "reference"
 
 sealed class Screen(var route: String) {
     object Home : Screen("home_screen")
+    object Create: Screen("create_screen")
     object Detail : Screen("detail_screen/{$KEY_DETAIL_SCREEN}") {
         fun setId(id: Int): String {
             return this.route.replace(
@@ -34,6 +36,10 @@ fun setupNavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.Home.route) {
         composable(route = Screen.Home.route) {
             HomeScreen(navHostController = navController)
+        }
+
+        composable(route = Screen.Create.route) {
+            CreatePostScreen(navHostController = navController)
         }
 
         composable(
